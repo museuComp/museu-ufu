aa7f7233cf6dce38447d7e721197e67e4361dabb
Ryan Costa <ryanpac16@gmail.com>
2026-03-02 20:04:14 -0300

    feat: create video detail layout

--- src/app/pages/videos/video-detail/video-detail.component.ts
+++ src/app/pages/videos/video-detail/video-detail.component.ts
@@ -1,10 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent, MatCardFooter } from "@angular/material/card";
+import { VideoPlayerComponent } from "@app/shared/components/video-player/video-player.component";
 
 @Component({
     selector: 'app-video-detail',
+    standalone: true,
     templateUrl: './video-detail.component.html',
     styleUrl: './video-detail.component.scss',
-    imports:[]
+    imports: [MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent, VideoPlayerComponent, MatCardFooter]
 })
 export class VideoDetailComponent{
 }
