ed92d87f19402c69188c1c937b5c47b1c71588e7
Ryan Costa <ryanpac16@gmail.com>
2026-03-03 22:44:04 -0300

    feat: add content to visitation videos page

--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -1,12 +1,23 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { MatCard } from "@angular/material/card";
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Observable } from 'rxjs';
+import { FirestoreVideosService, Video } from 'core/services/firestore-videos.service';
+import { AboutRoutingModule } from "../about/about-routing.module";
 
 @Component({
   selector: 'app-videos',
   standalone: true,
-  imports: [],
+  imports: [MatCard, CommonModule, AboutRoutingModule, NgOptimizedImage],
   templateUrl: './videos.component.html',
   styleUrl: './videos.component.scss'
 })
-export class VideosComponent {
+export class VideosComponent implements OnInit{
+  videosList$: Observable<Video[]>;
 
+  firestoreVideoService = inject(FirestoreVideosService);
+
+  ngOnInit(): void {
+    this.videosList$ = this.firestoreVideoService.getAllVideos();
+  }
 }
