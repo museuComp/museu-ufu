0b6d228385ed58074cac0a1be8cc0256d64155b8
Ryan Costa <ryanpac16@gmail.com>
2026-04-07 23:01:45 -0300

    feat: add magazine section to resources

--- src/app/pages/resources/resources.component.ts
+++ src/app/pages/resources/resources.component.ts
@@ -1,22 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { MatCard } from '@angular/material/card';
+import { MatCard, MatCardTitle } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 import { ContentSectionComponent } from "@app/shared/components/content-section/content-section.component";
 import { FirestoreNewsService, NewsPost } from 'core/services/firestore-news.service';
 import { FirestoreVideosService, Video } from 'core/services/firestore-videos.service';
 import { Observable } from 'rxjs';
+import { magazines } from '../magazine/magazine.mock';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resources',
-  imports: [ContentSectionComponent, RouterLink],
+  imports: [CommonModule, ContentSectionComponent, RouterLink, MatCard, MatCardTitle],
   templateUrl: './resources.component.html',
   styleUrl: './resources.component.scss'
 })
 export class ResourcesComponent implements OnInit {
   private videoService = inject(FirestoreVideosService);
   private newsService = inject(FirestoreNewsService);
+  
+  revistas = magazines;
 
   private limit = 3;
+
   personalitiesList$: Observable<NewsPost[]>;
   videosList$: Observable<Video[]>;
 
