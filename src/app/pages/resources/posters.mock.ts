568aeef0d63c86a5c9056c095464799f83182031
Ryan Pablo A. Costa <ryanpac16@gmail.com>
2026-06-03 23:18:30 -0300

    feat: add poster resource temp (#38)

---
+++ src/app/pages/resources/posters.mock.ts
@@ -0,0 +1,14 @@
+export const posters = [
+    {
+        'name': 'Alan Turing',
+        'path': 'alan-turing.png'
+    },
+    {
+        'name': 'Margaret Hamilton',
+        'path': 'margaret-hamilton.png'
+    },
+    {
+        'name': 'Vint Cerf',
+        'path': 'vint-cerf.png'
+    },
+]
--- src/app/pages/resources/resources.component.ts
+++ src/app/pages/resources/resources.component.ts
@@ -6,11 +6,13 @@ import { FirestoreNewsService, NewsPost } from 'core/services/firestore-news.ser
 import { FirestoreVideosService, Video } from 'core/services/firestore-videos.service';
 import { Observable } from 'rxjs';
 import { magazines } from '../magazine/magazine.mock';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { posters } from './posters.mock';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-resources',
-  imports: [CommonModule, ContentSectionComponent, RouterLink, MatCard, MatCardTitle],
+  imports: [CommonModule, ContentSectionComponent, RouterLink, MatCard, MatCardTitle, MatIcon],
   templateUrl: './resources.component.html',
   styleUrl: './resources.component.scss'
 })
@@ -19,6 +21,9 @@ export class ResourcesComponent implements OnInit {
   private newsService = inject(FirestoreNewsService);
   
   revistas = magazines;
+  posteres = posters;
+  showModal = false;
+  modalPath:string;
 
   private limit = 3;
 
@@ -29,4 +34,14 @@ export class ResourcesComponent implements OnInit {
     this.personalitiesList$ = this.newsService.getLimitedPersonalities(this.limit);
     this.videosList$ = this.videoService.getLimitedVideos(this.limit);
   }
+
+  openModal(path:string): void {
+    this.showModal = true;
+    this.modalPath = path;
+  }
+  closeModal():void {
+    this.showModal = false;
+    this.modalPath = "";
+  }
+
 }
