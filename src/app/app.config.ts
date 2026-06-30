10ef3313deba8feff3429581b74f770cb1f0fcb1
Ryan Costa <ryanpac16@gmail.com>
2026-06-29 23:10:40 -0300

    chore: transloco lib config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import {
   ApplicationConfig,
   DEFAULT_CURRENCY_CODE,
+  inject,
   isDevMode,
   LOCALE_ID,
+  provideAppInitializer,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter, TitleStrategy } from '@angular/router';
@@ -25,6 +27,8 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { getApps } from 'firebase/app';
 import { env } from '../../enviroment';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco, provideTranslocoLoader, TranslocoService } from '@jsverse/transloco';
 
 
 registerLocaleData(localePt);
@@ -77,5 +81,22 @@ export const appConfig: ApplicationConfig = {
         return getFirestore(app);
       }
     },
+    provideHttpClient(),
+
+    provideTransloco({
+        config: { 
+          availableLangs: ['pt-br', 'en', 'es'],
+          defaultLang: 'pt-br',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      }),
+
+      provideAppInitializer(() => {
+        const transloco = inject(TranslocoService);
+        transloco.load(transloco.getActiveLang());
+      })
   ],
 };
