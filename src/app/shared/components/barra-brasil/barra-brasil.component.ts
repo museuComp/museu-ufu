b4d23ce7a6d52880b96db35e622550e22248c2e6
Ryan Costa <ryanpac16@gmail.com>
2026-07-04 14:10:49 -0300

    refact: add i18n to gov.br bar and home page

--- src/app/shared/components/barra-brasil/barra-brasil.component.ts
+++ src/app/shared/components/barra-brasil/barra-brasil.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslocoService } from '@jsverse/transloco';
+import { AboutRoutingModule } from "@app/pages/about/about-routing.module";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-barra-brasil',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AboutRoutingModule],
   templateUrl: './barra-brasil.component.html',
   styleUrls: ['./barra-brasil.component.scss']
 })
 export class BarraBrasilComponent {
-  constructor() { }
+  readonly transloco = inject(TranslocoService);
+  private readonly router = inject(Router);
+
+  constructor() {}
+
+  changeLang(lang:string): void {
+    let url = this.router.url;
+    url = url.replace(this.transloco.getActiveLang(), lang);
+    this.router.navigate([url]);
+  }
 } 
