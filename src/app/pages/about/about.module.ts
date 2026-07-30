a95d6f255e4cd58065711f81805b292f52c5c5cb
Ryan Pablo A. Costa <ryanpac16@gmail.com>
2026-07-30 16:04:38 -0300

    Feature/i18n: adicionando suporte à multiliguagem ao museu virtual (#49)
    * hotfix: add donation button to nav bar
    
    * chore: transloco lib config
    
    * refact: add i18n to breadcrumb and routes
    
    * fix: add english and spanish translation
    
    * refact: add i18n to header and menu
    
    * refact: add i18n to perfis nav  and footer
    
    * refact: add i18n to gov.br bar and home page
    
    * refact: add i18n to nroms and about page
    
    * refactor: create games data list
    
    * chore: add nes.css dependency
    
    * feat: add reference images to games cards
    
    * feat: add arcade style with nes.css to games page
    
    * refact: add i18n to virtual tour
    
    * refactor: add i18n to virtual tour
    
    ---------
    
    Co-authored-by: Douglas <douglas.nascimento@ufu.br>

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+  import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
